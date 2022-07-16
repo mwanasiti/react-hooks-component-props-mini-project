603026f490a6590c56badab26d1140d8c74549a1
Mwanasiti <ali.mariam.mwanasiti@gmail.com>
2022-07-16 17:45:07 +0300

    emo

---
+++ src/components/Article.js
@@ -0,0 +1,45 @@
+// import React from "react";
+
+
+// function Article({ title, date = "January 1, 1970", preview, minutes }){
+
+//     return (
+//         <article>
+//           <h3>{title}</h3>
+//           <small>
+//             {date}
+//           </small>
+//           <p>{preview}</p>
+//         </article>
+//       );
+// }
+
+import React from "react";
+
+function EmojisList(minutes) {
+  const interval = minutes < 30 ? 5 : 10;
+  const emoji = minutes < 30 ? "☕️" : "🍱";
+
+  let emojis = "";
+  for (let i = 0; i < minutes; i += interval) {
+    emojis += emoji;
+  }
+
+  return emojis;
+}
+
+function Article({ title, date = "January 1, 1970", preview, minutes }) {
+  const emojis = EmojisList(minutes);
+
+  return (
+    <article>
+      <h3>{title}</h3>
+      <small>
+        {date} • {emojis} {minutes} min read
+      </small>
+      <p>{preview}</p>
+    </article>
+  );
+}
+
+export default Article;
